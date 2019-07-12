366f68d82a66941768d489f113364e8d99568753
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-12 20:22:55 +0300

    render guessed words component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import GuessedWords from "./GuessedWords"
+import React from 'react';
+
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GuessedWords guessedWords={[{ guessedWord: "aaa", letterMatchCount: 5 }]} />
     </div>
   );
 }
