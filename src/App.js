d3a7ea969def2be7c177c5a8b27b3d4df906b601
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-12 20:24:54 +0300

    render Congrats component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
+import Congrats from "./Congrats"
 import GuessedWords from "./GuessedWords"
 import React from 'react';
 
 function App () {
   return (
     <div className="App">
+      <h1>Jotto</h1>
+      <Congrats success={false} />
       <GuessedWords guessedWords={[{ guessedWord: "aaa", letterMatchCount: 5 }]} />
     </div>
   );
