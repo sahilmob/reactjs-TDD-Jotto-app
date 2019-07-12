645fc2fc474906d2e4e6d602378f1a45941aeb7c
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-12 16:23:11 +0300

    build congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -3,12 +3,19 @@ import React from 'react'
 /**
  * Functional react component for congratulatory message.
  * @function
+ * @param {object} props - React props.
  * @returns {JSX.Element} - Render component (or null if `success` pros is false).
  */
-export default function Congrats () {
-  return (
-    <div>
-
-    </div>
-  )
+export default function Congrats (props) {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! you guessed the word!
+        </span>
+      </div>
+    )
+  } else {
+    return <div data-test="component-congrats" />
+  }
 }
