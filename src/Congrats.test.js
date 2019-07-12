2e6e7243ffea1332df149df9714ca236ec334a3c
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-12 16:23:20 +0300

    test component rendering

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -18,11 +18,18 @@ const setup = (props = {}) => {
   return shallow(<Congrats {...props} />)
 }
 it("should renders without error", () => {
-
+  const wrapper = setup()
+  const component = findByAttr(wrapper, "component-congrats")
+  expect(component.length).toBe(1)
 })
 it("should renders no text when `success prop is false`", () => {
-
+  const wrapper = setup({ success: false })
+  const component = findByAttr(wrapper, "component-congrats")
+  expect(component.text()).toBe("")
 })
 it("renders non-empty congrats message when `success` message is true", () => {
 
+  const wrapper = setup({ success: true })
+  const message = findByAttr(wrapper, "congrats-message")
+  expect(message.text().length)
 })
