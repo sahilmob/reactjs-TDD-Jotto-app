183c84a4f06142d7e9511b4222d7ae871b4349eb
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-12 16:47:31 +0300

    add check props utils

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,9 +1,16 @@
+import checkPropsTypes from "check-prop-types";
+
 /**
  * Return node(s) with the given data-test attr.
  * @param {ShallowWrapper} wrapper -  Enzyme shallow wrapper
  * @param {*} val - Value of data-test attr. for search.
- * @returns {ShallowWraper}
+ * @returns {ShallowWrapper}
  */
 export const findByAttr = (wrapper, val) => {
   return wrapper.find(`[data-test='${ val }']`)
+}
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropsTypes(component.propTypes, conformingProps, "props", component.name)
+  expect(propError).toBeUndefined()
 }
