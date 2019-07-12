a9d24d420c64821546dd9233ba8f0453069fc2a0
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-12 16:02:09 +0300

    create component shell

---
+++ src/Congrats.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} - Render component (or null if `success` pros is false).
+ */
+export default function Congrats () {
+  return (
+    <div>
+
+    </div>
+  )
+}
