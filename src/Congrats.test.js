a14e97f651d166d1ddd85f8cf7ffc5a352728f46
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-12 16:02:17 +0300

    testing basic setup

---
+++ src/Congrats.test.js
@@ -0,0 +1,8 @@
+import Enzyme, { shallow } from "enzyme"
+
+import Congrats from "./Congrats";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import React from "react";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
