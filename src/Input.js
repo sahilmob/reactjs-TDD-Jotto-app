cb75901d3da4ac3cdf2e2262f3693632926003b5
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-19 15:35:35 +0300

    implement input component

--- src/Input.js
+++ src/Input.js
@@ -4,16 +4,26 @@ import { connect } from "react-redux"
 
 class Input extends Component {
   render () {
+    const { success } = this.props;
+
     return (
-      <div>
-        <button>Hii</button>
+      <div data-test="component-input">
+        {success ? null
+          : <>
+            <form className="form-inline">
+              <input type="text" data-test="input-box" className="mb-2 mx-sm-3" id="word-guess" placeholder="Enter guess" />
+              <button data-test="submit-button" className="btn btn-primary" type="submit">Submit</button>
+            </form>
+          </>}
       </div>
     )
   }
 }
 
 const mapStateToProps = state => {
-  return {}
+  return {
+    success: state.success
+  }
 }
 
 export default connect(mapStateToProps)(Input)
