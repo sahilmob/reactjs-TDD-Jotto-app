0900f84c224d68929a3e588b1a74ea49de23245b
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-20 17:51:29 +0300

    delete

--- src/actions/index.test.js
+++
@@ -1,9 +0,0 @@
-import { actionTypes, correctGuess } from "./"
-
-describe("correctGuess", () => {
-  it("should returns an action with `CORRECT_GUESS` action type", () => {
-    const action = correctGuess()
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS })
-
-  })
-})
