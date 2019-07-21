c257c8df9e0ca852653f58f6a23f075479656a7d
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-21 20:24:50 +0300

    implement secretWordReducer

--- src/reduces/secretWordReducer.js
+++ src/reduces/secretWordReducer.js
@@ -1,3 +1,15 @@
-export default (state = null, action) => {
-  return state
+import { actionTypes } from "../actions"
+
+/**
+ * @function secretWordReducer
+ * @param {string} state - State before reducer
+ * @param {object} action - Action sent to reducer.
+ * @returns {string} - New state (secret word).
+ */
+export default (state = null, { type, payload }) => {
+  switch (type) {
+    case actionTypes.SET_SECRET_WORD:
+      return payload
+    default: return state
+  }
 }
