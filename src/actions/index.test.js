7df13221df741b848b8500a79ae09389aecf9bdb
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-21 20:25:22 +0300

    test secretWordReducer and getSecretWord action creator

---
+++ src/actions/index.test.js
@@ -0,0 +1,28 @@
+import { getSecretWord } from "./"
+import moxios from "moxios";
+import { storeFactory } from "../../test/testUtils"
+
+describe("getSecretWord action creator", () => {
+  beforeEach(() => {
+    moxios.install();
+  })
+  afterEach(() => {
+    moxios.uninstall()
+  })
+  it("adds response word to state", () => {
+    const secretWord = 'party'
+    const store = storeFactory()
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({
+        status: 200,
+        response: secretWord
+      })
+    })
+    return store.dispatch(getSecretWord()).then(() => {
+      const newState = store.getState()
+      expect(newState.secretWord).toBe(secretWord)
+    })
+  })
+})
