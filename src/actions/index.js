f3e9e967d0952413a32c18ebf826bef3c4e06975
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-07-21 20:24:30 +0300

    add getSecretWord action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
+import axios from "axios"
 import { getLetterMatchCount } from "../helpers"
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
-  GUESS_WORD: 'GUESS_WORD'
+  GUESS_WORD: 'GUESS_WORD',
+  SET_SECRET_WORD: 'SET_SECRET_WORD'
 
 };
 
@@ -23,5 +25,16 @@ export const guessWord = (guessedWord) => {
     if (guessedWord === secretWord) {
       dispatch({ type: actionTypes.CORRECT_GUESS })
     }
+  }
+}
+
+export const getSecretWord = () => {
+  return (dispatch, getState) => {
+    return axios.get("http://localhost:3030").then(res => {
+      dispatch({
+        type: actionTypes.SET_SECRET_WORD,
+        payload: res.data
+      })
+    });
   }
 }
