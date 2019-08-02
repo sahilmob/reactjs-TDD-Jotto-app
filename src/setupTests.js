87c735e1185569f06f7d8485e513d662ae3222a2
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2019-08-02 15:10:35 +0300

    disable component lifecycle methods

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,7 +4,7 @@ import EnzymeAdapter from "enzyme-adapter-react-16";
 
 const adapter = new EnzymeAdapter()
 
-Enzyme.configure({ adapter })
+Enzyme.configure({ adapter, disableLifecycleMethods: true })
 global.Enzyme = Enzyme
 global.adapter = adapter
 global.shallow = shallow
